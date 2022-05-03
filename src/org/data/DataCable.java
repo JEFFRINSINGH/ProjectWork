8e7e327701730af570970dada984a01a1d38214a
JEFFRINSINGH <jeffrinsingh05@gmail.com>
2022-05-03 11:32:23 +0530

    Third update

--- src/org/data/DataCable.java
+++ src/org/data/DataCable.java
@@ -4,6 +4,10 @@ public class DataCable {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		System.out.println("Pythan");
+		System.out.println("TestNG");
+		System.out.println("Cucumber");
+		System.out.println("SQL");
+		System.out.println("API");
 	}
 
 }
